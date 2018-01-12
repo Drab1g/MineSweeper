45f8c05e1035714dfdd1b6db7cd044cb72d6aeb7
Ali Bekkouche <ali.bekkouche@etud.univ-angers.fr>
2018-01-12 19:21:05 +0100

    exploding empty areas feature fixed

--- app/src/main/java/com/example/usrlocal/minesweeper/FrontSquareAdapter.java
+++ app/src/main/java/com/example/usrlocal/minesweeper/FrontSquareAdapter.java
@@ -1,12 +1,16 @@
 package com.example.usrlocal.minesweeper;
 
 import android.content.Context;
+import android.support.v4.content.res.TypedArrayUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FrontSquareAdapter extends BaseAdapter {
 
@@ -66,7 +70,7 @@ public class FrontSquareAdapter extends BaseAdapter {
 
           // explode empty area
           case 0:
-            //clearedSquares-= number of exploded squares
+            explodeEmptyArea(position,parent);
             break;
 
           // reveal what is underneath  and check if game is completed
@@ -110,5 +114,97 @@ public class FrontSquareAdapter extends BaseAdapter {
     return position;
   }
 
+  public int explodeEmptyArea(int clickedSquare, ViewGroup grid) {
+
+    List<Integer> emptySquaresArea = new ArrayList<Integer>(); // storing in this list all empty squares
+    //position that are within the same area as the cliquedSquare
+
+    int currentEvaluation = 0;
+    int currentPosition = clickedSquare;
+    boolean continuer = true;
+
+    emptySquaresArea.add(currentPosition);
+    grid.getChildAt(currentPosition).setVisibility(View.GONE);
+
+    while (continuer || currentEvaluation < emptySquaresArea.size()) {
+      continuer = false;
+      currentPosition = emptySquaresArea.get(currentEvaluation);
+
+      int col = currentPosition % gameBoard.getCols();
+      int row = (currentPosition - col) / gameBoard.getCols();
+      boolean left = col > 0;
+      boolean right = col + 1 < gameBoard.getCols();
+      boolean top = row > 0;
+      boolean bottom = row + 1 < gameBoard.getRows();
+
+      if (top) {
+        int upPos = currentPosition - gameBoard.getCols();
+        grid.getChildAt(upPos).setVisibility(View.GONE);
+        if (gameBoard.getMines(upPos) == 0 && !emptySquaresArea.contains(upPos)) {
+          continuer = true;
+          emptySquaresArea.add(upPos);
+        }
+        if (left) {
+          int upleftPos = currentPosition - gameBoard.getCols() - 1;
+          grid.getChildAt(upleftPos).setVisibility(View.GONE);
+          if (gameBoard.getMines(upleftPos) == 0 && !emptySquaresArea.contains(upleftPos)) {
+            continuer = true;
+            emptySquaresArea.add(upleftPos);
+          }
+        }
+        if (right) {
+          int uprightPos = currentPosition - gameBoard.getCols() + 1;
+          grid.getChildAt(uprightPos).setVisibility(View.GONE);
+          if (gameBoard.getMines(uprightPos) == 0 && !emptySquaresArea.contains(uprightPos)) {
+            continuer = true;
+            emptySquaresArea.add(uprightPos);
+          }
+        }
+      }
+      if (bottom) {
+        int downPos = currentPosition + gameBoard.getCols();
+        grid.getChildAt(downPos).setVisibility(View.GONE);
+        if (gameBoard.getMines(downPos) == 0 && !emptySquaresArea.contains(downPos)) {
+          continuer = true;
+          emptySquaresArea.add(downPos);
+        }
+        if (left) {
+          int downleftPos = currentPosition + gameBoard.getCols() - 1;
+          grid.getChildAt(downleftPos).setVisibility(View.GONE);
+          if (gameBoard.getMines(downleftPos) == 0 && !emptySquaresArea.contains(downleftPos)) {
+            continuer = true;
+            emptySquaresArea.add(downleftPos);
+          }
+        }
+        if (right) {
+          int downrightPos = currentPosition + gameBoard.getCols() + 1;
+          grid.getChildAt(downrightPos).setVisibility(View.GONE);
+          if (gameBoard.getMines(downrightPos) == 0 && !emptySquaresArea.contains(downrightPos)) {
+            continuer = true;
+            emptySquaresArea.add(downrightPos);
+          }
+        }
+      }
+      if (left) {
+        int leftPos = currentPosition - 1;
+        grid.getChildAt(leftPos).setVisibility(View.GONE);
+        if (gameBoard.getMines(leftPos) == 0 && !emptySquaresArea.contains(leftPos)) {
+          continuer = true;
+          emptySquaresArea.add(leftPos);
+        }
+      }
+
+      if (right) {
+        int rightPos = currentPosition + 1;
+        grid.getChildAt(rightPos).setVisibility(View.GONE);
+        if (gameBoard.getMines(rightPos) == 0 && !emptySquaresArea.contains(rightPos)) {
+          continuer = true;
+          emptySquaresArea.add(rightPos);
+        }
+      }
+      currentEvaluation++;
+    }
+    return 1;
+  }
 }
 
